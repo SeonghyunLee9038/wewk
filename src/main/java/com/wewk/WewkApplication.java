aebf6ec5ad241a7868fc7d9b2fa9d867fd80281f
DESKTOP-CON0HK4\sangs <shlee@sangs.co.kr>
2023-03-23 16:38:59 +0900

    Initial commit

---
+++ src/main/java/com/wewk/WewkApplication.java
@@ -0,0 +1,13 @@
+package com.wewk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WewkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WewkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wewk/WewkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wewk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WewkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
